dc30ab016f0783fcf7f67e7609cc50705f01b71f
arunthampi <arun.thampi@gmail.com>
2015-10-12 21:55:49 -0700

    More resilient redisclient
    * In Heroku, sometimes it takes a while for the Redis server to come up (especially when using the “Deploy to Heroku” button), so retry auth’ing
    * Wrap the redisclient in a mutex

--- redisclient/client.go
+++ redisclient/client.go
@@ -1,16 +1,23 @@
 package redisclient
 
 import (
-	"fmt"
 	"net/url"
 	"os"
+	"sync"
+	"time"
+
+	log "github.com/zerobotlabs/relax/Godeps/_workspace/src/github.com/Sirupsen/logrus"
 
 	"github.com/zerobotlabs/relax/Godeps/_workspace/src/gopkg.in/redis.v3"
 )
 
 var redisClient *redis.Client
+var mu sync.Mutex
 
 func Client() *redis.Client {
+	mu.Lock()
+	defer mu.Unlock()
+
 	if redisClient == nil {
 		password := ""
 		host := ""
@@ -30,12 +37,29 @@ func Client() *redis.Client {
 			password = os.Getenv("REDIS_PASSWORD")
 		}
 
-		fmt.Printf("host: %s userInfo: %s\n", host, password)
 		redisClient = redis.NewClient(&redis.Options{
 			Addr:     host,
 			Password: password,
 			DB:       0,
 		})
+
+		result, err := redisClient.Ping().Result()
+
+		for err != nil || result != "PONG" {
+			log.WithFields(log.Fields{
+				"host":   host,
+				"err":    err,
+				"result": result,
+			}).Error("retrying connecting to redis host, failed credentials")
+
+			time.Sleep(5 * time.Second)
+			result, err = redisClient.Ping().Result()
+		}
+
+		log.WithFields(log.Fields{
+			"host":   host,
+			"result": result,
+		}).Info("connected to redis host")
 	}
 
 	return redisClient
